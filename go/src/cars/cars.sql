1f7a71e05f6f4b6becfac09910003401e66f27b1
SIMarkov <116885459+SIMarkov@users.noreply.github.com>
2024-06-08 09:14:36 -0400

    Update cars.sql

--- go/src/cars/cars.sql
+++ go/src/cars/cars.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE cars;
 
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'Slavil1991';
+CREATE USER 'root'@'localhost' IDENTIFIED BY 'SM';
 GRANT ALL PRIVILEGES ON cars.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
